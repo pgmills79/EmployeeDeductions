a32191dabe70d95d0c82d4ef5b84abccc086b97d
Paul Mills <Mills.Paul@mayo.edu>
2022-04-15 19:13:08 -0400

    refactored: some calculations in Utility.cs updated:  tests in CalculationTests.cs

--- Deductions.Domain/Utility.cs
+++ Deductions.Domain/Utility.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Deductions.Domain
 {
     public static class Utility
     {
+        public const int NumberOfPaychecks = 26;
+        public const int DependentAnnualCost = 500;
+        private const int EmployeeAnnualCost = 1000;
+        private const decimal DiscountPercent = 0.10m;
+        private const char ApplyDiscountLetter = 'A';
+        
         public static bool DoesNameStartWithLetter(string nameInput, char letterToCheck)
         {
             if (string.IsNullOrEmpty(nameInput)) return false;
@@ -12,11 +20,43 @@ namespace Deductions.Domain
                    && nameInput.TrimStart().StartsWith(letterToCheck.ToString(), StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public static decimal GetTotalCostDependents(int numberOfDependents = 0)
+        public static decimal GetDependentsPaycheckDeductionAmount(List<string> dependents)
         {
-            if (numberOfDependents <= 0) return 0;
+            var deductionAmount = 0.00m;
 
-            return numberOfDependents * 500;
+            //if no dependents found just return zero
+            if (dependents?.Any() != true) return 0;
+
+            //add up the dependents amounts
+            foreach (var dependent in dependents)
+            {
+                if (DoesNameStartWithLetter(dependent, ApplyDiscountLetter))
+                {
+                    deductionAmount += GetDependentDiscountAmount();
+                }
+                else
+                {
+                    deductionAmount += GetDependentDeductionAmount();
+                }
+            }
+            
+            //does name get
+            return deductionAmount;
+        }
+
+        public static decimal GetDependentDeductionAmount()
+        {
+            return Convert.ToDecimal(DependentAnnualCost / NumberOfPaychecks);
+        }
+
+        public static decimal GetDependentDiscountAmount()
+        {
+            return Convert.ToDecimal( (DependentAnnualCost - DiscountPercent * DependentAnnualCost) / NumberOfPaychecks);
+        }
+        
+        public static decimal GetEmployeeDeductionAmount()
+        {
+            return Convert.ToDecimal(EmployeeAnnualCost / NumberOfPaychecks);
         }
     }
 }
--- Deductions.UnitTests/CalculationTests.cs
+++ Deductions.UnitTests/CalculationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Deductions.Domain;
 using Xunit;
 
@@ -6,6 +7,20 @@ namespace Deductions.UnitTests
 {
     public class CalculationTests
     {
+        [Fact]
+        public void GetDependentDeductionAmount_Should_Return_Correct_Amount()
+        {
+            //arrange
+            var expectedResult = Convert.ToDecimal(Utility.DependentAnnualCost / Utility.NumberOfPaychecks);
+            
+            //action
+            var actualResult = Utility.GetDependentDeductionAmount();
+
+            //assert
+            Assert.True(expectedResult.Equals(actualResult));
+        }
+        
+        
         [Theory]
         [InlineData("John Doe", 'A', false)]
         [InlineData("Aaron Mills", 'A', true)]
@@ -26,22 +41,33 @@ namespace Deductions.UnitTests
             //assert
            Assert.True(nameStartsWithInputLetter.Equals(expectedResult));
         }
-        
+
         [Theory]
-        [InlineData(3, 1500.00)]
-        [InlineData(2, 1000)]
-        [InlineData(50, 25000)]
-        [InlineData(0, 0)]
-        [InlineData(-10, 0)]
-        public void Get_Total_Cost_Dependents_Should_Return_Correct_Amount(int numberDependents, decimal  expectedAmount)
+        [MemberData(nameof(TestGetDependentsTestData))]
+        public void Get_Dependents_Paycheck_Deduction_Amount(List<string> dependents, decimal expectedAmount)
         {
             //arrange
 
             //action
-            var totalCost = Utility.GetTotalCostDependents(numberDependents);
+            var paycheckDeductionAmount = Utility.GetDependentsPaycheckDeductionAmount(dependents);
 
             //assert
-            Assert.True(totalCost.Equals(expectedAmount));
+            Assert.True(paycheckDeductionAmount.Equals(expectedAmount));
         }
+        
+        public static IEnumerable<object[]> TestGetDependentsTestData =>
+            new List<object[]>
+            {
+                //No Discounts for these dependents
+                new object[] { new List<string> { "John Doe", "Paul M", "Tom Jerry" }, Utility.GetDependentDeductionAmount() * 3 },
+                new object[] { new List<string> { "John Doe", "Paul M" }, Utility.GetDependentDeductionAmount() * 2 },
+                //10% discount on 1 dependent
+                new object[] { new List<string> { "Adam Bob" }, Utility.GetDependentDiscountAmount() * 1 },
+                //10% discount on 1 dependent
+                new object[] { new List<string> { "Adam Bob", "John Doe" }, Utility.GetDependentDiscountAmount() * 1 + Utility.GetDependentDeductionAmount() },
+                //10% discount on 2 dependents
+                new object[] { new List<string> { "Adam Bob", "Aaron Willis" }, Utility.GetDependentDiscountAmount() * 2 },
+                
+            };
     }
 }
