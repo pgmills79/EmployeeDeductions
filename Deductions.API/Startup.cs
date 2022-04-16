593e8a1701701ac5c489900b8f0115bffabbb711
Paul Mills <Mills.Paul@mayo.edu>
2022-04-16 14:39:22 -0400

    cleanup:  unused directives in Program.cs and Startup.cs

--- Deductions.API/Startup.cs
+++ Deductions.API/Startup.cs
@@ -1,22 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using Deductions.Domain;
 using Deductions.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace Deductoins.API
+namespace Deductions.API
 {
     public class Startup
     {
