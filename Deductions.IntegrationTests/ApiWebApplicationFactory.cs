10f6860378f8b1d66fee0bfbb6bb4f0535f41cbe
Paul Mills <Mills.Paul@mayo.edu>
2022-04-17 01:08:03 -0400

    add:  integration test project

---
+++ Deductions.IntegrationTests/ApiWebApplicationFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Deductions.API;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace Deductions.IntegrationTests
+{
+    public class ApiWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        private IConfiguration Configuration { get; set; }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+
+            builder.ConfigureAppConfiguration(config =>
+            {
+                Configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .Build();
+
+                config.AddConfiguration(Configuration);
+               
+            });
+
+        }
+
+    }
+}
